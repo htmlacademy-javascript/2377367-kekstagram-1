b247581f70e1ab4bc9572d982cdedeb126056e02
MariiaShel <mix.laughter@mail.ru>
2023-09-05 18:58:23 +0300

    Добавила функции

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+const isLessOrEqual = (string, length) => string.length <= length;
+
+const isPalindrom = (string) => {
+  const tempString = string
+    .toLowerCase()
+    .replaceAll(' ', '');
+  let reverseString = '';
+  for (let i = tempString.length - 1; i >= 0; i--) {
+    reverseString += tempString.at(i)
+  }
+
+  return tempString === reverseString;
+}
+
+const extactNumber = (string) => {
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
+  return parseInt(result, 10);
+}
+
+
+const myPadStart = (string, minLength, pad) => {
+  const actualPad = minLength - string.length;
+
+  if (actualPad <= 0) {
+    return string;
+  }
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
+}
